fde9a19288a5b20a8aea77f317a4deea5d32d4c6
Reynold Osei Adade <reynold@walulel.com>
2021-07-12 16:04:07 +0000

    banner started

---
+++ components/banner.js
@@ -0,0 +1,11 @@
+const banner  = () =>  (
+    <div className="h-64 w-full ">
+        <div className="flex justify-center items-center w-full h-full flex-col">
+            <div className="uppercase text-gray-400" id="intro">Hello I AM</div>
+           <div className="uppercase font-bold text-lg" id="name">reynold osei adade</div>
+           <div className="p-1 text-xs " id="subtitle">Frontend Enthusiast, tinkerer, Clientside researcher</div>
+        </div>
+    </div>
+)
+
+export default banner
